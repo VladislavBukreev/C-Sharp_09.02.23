8f6ada5f5b34015c38dcaff0c5494ed8a6a1cee3
VladislavBukreev <bukr7@mail.ru>
2023-02-12 21:12:08 +1000

    New Examples

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Нахождение максимума с помощью функции
+Max - метод(функция) которая ищет максимальное значение
+Return служит для выхода из функции. Оператор предназначен для возврата значения из метода.
+*/
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 1472;
+int b2 = 2311;
+int c2 = 33;
+int a3 = 113;
+int b3 = 23;
+int c3 = 313;
+
+/*
+int max1 = Max(a1,b1,c1);
+int max2 = Max(a2,b2,c2);
+int max3 = Max(a3,b3,c3);
+int max = Max(max1,max2,max3);
+
+Это равносильно той строке что снизу
+*/
+ int max = Max(
+    Max(a1, b1 ,c1), 
+    Max(a2, b2, c2), 
+    Max(a3,b3, c3));
+
+Console.WriteLine (max);
