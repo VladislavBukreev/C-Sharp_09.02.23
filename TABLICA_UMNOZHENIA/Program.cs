be1f0ba8081b6525d866bcdcc7a1441ea4c57904
Vladislav <123633672+VladislavBukreev@users.noreply.github.com>
2023-03-23 22:31:42 +1000

    Add files via upload

---
+++ TABLICA_UMNOZHENIA/Program.cs
@@ -0,0 +1,22 @@
+﻿// Таблица умножения
+int n = Convert.ToInt32(Console.ReadLine()); 
+int[, ] matrix = new int[n, n];
+for(int i = 0; i < n; i++)
+{
+    for (int j = i; j < n; j++)
+        {
+            matrix[i, j] = (i + 1) * (j + 1);
+            matrix[j, i] = (i + 1) * (j + 1);
+        }
+        Console.WriteLine();
+}
+for(int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+}
+// O(п^2 / 2)  
